fdb72c37c4ea8d5d65885bf38d18c483dc3e19f6
CTO & Developer <github@index-performance.com>
2025-01-18 21:41:29 +0100

    project 'started ...'

---
+++ ipc_ATAS_indicators/ipc_ATAS_indicators/AssemblyInfo1.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("additional ATAS-Indicators")]
+[assembly: AssemblyCopyright("Copyright © 2025 by index-performance.com")]
+[assembly: AssemblyTrademark("by index-performance.com")]
+[assembly: AssemblyCulture("")]
+
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("94296ca9-b678-4f1b-b75e-10731c13e288")]
