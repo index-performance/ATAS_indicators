2d14cda41f7b2058fb33aa34909e06a48e93a255
sql-trade <sql-trade_github@sql-fabrik.de>
2025-09-22 22:31:13 +0200

    'ipc_write2File'   updated

--- ipc_ATAS_indicators/ipc_ATAS_indicators/ipc_write2File.cs
+++ ipc_ATAS_indicators/ipc_ATAS_indicators/ipc_write2File.cs
@@ -28,6 +28,13 @@
         public decimal bXmaxVol_Pi__bid;
         public decimal bXmaxVol_Pi__ask;
 
+        public decimal bXmaxPosDelta_Pi__Price;
+        public decimal bXmaxPosDelta_Pi__Volume;
+        public decimal bXmaxNegDelta_Pi__Price;
+        public decimal bXmaxNegDelta_Pi__Volume;
+        public decimal bXvalArea__High;
+        public decimal bXvaLArea__Low;
+
         #endregion
         //--------------
 
@@ -59,16 +66,18 @@
                      + @"_"  + ChartInfo.ChartType + "." + ChartInfo.TimeFrame.Replace(@"/", ".").Replace(@"\", ".")
                      + @"__" + DateTime.Now.ToString("yyyyMMdd")
                      + @"_"  + DateTime.Now.ToString("HHmmss")
-                     + @"_ticks.txt";
+                     + @".txt";
 
             sw = File.AppendText(pathFile);
             sw.WriteLine(InstrumentInfo.Instrument + @"_" + ChartInfo.ChartType + @"." + ChartInfo.TimeFrame + @"__data");
-            sw.WriteLine("Instrument | Date | UTC_Time | LastTime "
-                       + "|     Open |     High |      Low |    Close "
-                       + "|   Volume |    Delta | maxDelta | minDelta "
-                       + "|    Ticks "
-                       + "| POCprice |   POCvol |   POCmax |   POCmin"
-                       );
+            sw.WriteLine( "Instrument | Date | UTC_Time | LastTime "
+                        + "|     Open |     High |      Low |    Close "
+                        + "|   Volume |        Δ |     maxΔ |     minΔ "  //-- \u0394 => Δ Delta
+                        + "|    Ticks "
+                        + "| POCprice |   POCvol |   POCmax |   POCmin "
+                        + "| maxPosΔp | maxPosΔv | maxNegΔp | maxNegΔv"   //-- max.min positives.negatives Delta am Preis
+                        //    p=Price   v=Volume                                              
+                        );
 
             sw.Close();
             sw.Dispose();
@@ -88,29 +97,46 @@
             bXmaxVol_Pi__bid = volumeInfo.Bid;
             bXmaxVol_Pi__ask = volumeInfo.Ask;
             //----
+            PriceVolumeInfo maxPosDelta_Pi = candle.MaxPositiveDeltaPriceInfo;
+            bXmaxPosDelta_Pi__Price  = maxPosDelta_Pi.Price;
+            bXmaxPosDelta_Pi__Volume = maxPosDelta_Pi.Volume;
+            //----
+            PriceVolumeInfo maxNegDelta_Pi = candle.MaxNegativeDeltaPriceInfo;
+            bXmaxNegDelta_Pi__Price  = maxNegDelta_Pi.Price;
+            bXmaxNegDelta_Pi__Volume = maxNegDelta_Pi.Volume;
+            //----
+            ValueArea valArea = candle.ValueArea;
+            bXvalArea__High = valArea.ValueAreaHigh;
+            bXvaLArea__Low  = valArea.ValueAreaLow;
 
 
-            sw = File.AppendText(pathFile);
-            sw.WriteLine(InstrumentInfo.Instrument + " | " + candle.Time.ToString("dd.MM.yyyy")
-                                                   + " | " + candle.Time.ToString("HH:mm:ss")
-                                                   + " | " + candle.LastTime.ToString("HH:mm:ss")
+            sw = File.AppendText(pathFile);  
+            sw.WriteLine(         InstrumentInfo.Instrument 
+                        + " | " + candle.Time.ToString("dd.MM.yyyy")
+                        + " | " + candle.Time.ToString("HH:mm:ss")
+                        + " | " + candle.LastTime.ToString("HH:mm:ss")
 
-                                                   + " | " + Strings.StrDup(8 - candle.Open.ToString().Length, @" ")     + candle.Open
-                                                   + " | " + Strings.StrDup(8 - candle.High.ToString().Length, @" ")     + candle.High
-                                                   + " | " + Strings.StrDup(8 - candle.Low.ToString().Length, @" ")      + candle.Low
-                                                   + " | " + Strings.StrDup(8 - candle.Close.ToString().Length, @" ")    + candle.Close     // -- format "####0,00" 
+                        + " | " + Strings.StrDup(8 - candle.Open.ToString().Length, @" ")         + candle.Open
+                        + " | " + Strings.StrDup(8 - candle.High.ToString().Length, @" ")         + candle.High
+                        + " | " + Strings.StrDup(8 - candle.Low.ToString().Length, @" ")          + candle.Low
+                        + " | " + Strings.StrDup(8 - candle.Close.ToString().Length, @" ")        + candle.Close     // -- format "####0,00" 
 
-                                                   + " | " + Strings.StrDup(8 - candle.Volume.ToString().Length, @" ")   + candle.Volume
-                                                   + " | " + Strings.StrDup(8 - candle.Delta.ToString().Length, @" ")    + candle.Delta
-                                                   + " | " + Strings.StrDup(8 - candle.MaxDelta.ToString().Length, @" ") + candle.MaxDelta
-                                                   + " | " + Strings.StrDup(8 - candle.MinDelta.ToString().Length, @" ") + candle.MinDelta
-                                                   + " | " + Strings.StrDup(8 - candle.Ticks.ToString().Length, @" ")    + candle.Ticks
+                        + " | " + Strings.StrDup(8 - candle.Volume.ToString().Length, @" ")       + candle.Volume
+                        + " | " + Strings.StrDup(8 - candle.Delta.ToString().Length, @" ")        + candle.Delta
+                        + " | " + Strings.StrDup(8 - candle.MaxDelta.ToString().Length, @" ")     + candle.MaxDelta
+                        + " | " + Strings.StrDup(8 - candle.MinDelta.ToString().Length, @" ")     + candle.MinDelta
 
-                                                   + " | " + Strings.StrDup(8 - bXmaxVol_Pi__Price.ToString().Length, @" ")  + bXmaxVol_Pi__Price   // -- POCprice 
-                                                   + " | " + Strings.StrDup(8 - bXmaxVol_Pi__Volume.ToString().Length, @" ") + bXmaxVol_Pi__Volume
-                                                   + " | " + Strings.StrDup(8 - bXmaxVol_Pi__bid.ToString().Length, @" ")    + bXmaxVol_Pi__bid
-                                                   + " | " + Strings.StrDup(8 - bXmaxVol_Pi__ask.ToString().Length, @" ")    + bXmaxVol_Pi__ask
+                        + " | " + Strings.StrDup(8 - candle.Ticks.ToString().Length, @" ")        + candle.Ticks
 
+                        + " | " + Strings.StrDup(8 - bXmaxVol_Pi__Price.ToString().Length, @" ")  + bXmaxVol_Pi__Price   // -- POCprice 
+                        + " | " + Strings.StrDup(8 - bXmaxVol_Pi__Volume.ToString().Length, @" ") + bXmaxVol_Pi__Volume
+                        + " | " + Strings.StrDup(8 - bXmaxVol_Pi__bid.ToString().Length, @" ")    + bXmaxVol_Pi__bid
+                        + " | " + Strings.StrDup(8 - bXmaxVol_Pi__ask.ToString().Length, @" ")    + bXmaxVol_Pi__ask
+
+                        + " | " + Strings.StrDup(8 - bXmaxPosDelta_Pi__Price.ToString().Length, @" ")   + bXmaxPosDelta_Pi__Price    // -- Δ 
+                        + " | " + Strings.StrDup(8 - bXmaxPosDelta_Pi__Volume.ToString().Length, @" ")  + bXmaxPosDelta_Pi__Volume
+                        + " | " + Strings.StrDup(8 - bXmaxNegDelta_Pi__Price.ToString().Length, @" ")   + bXmaxNegDelta_Pi__Price
+                        + " | " + Strings.StrDup(8 - bXmaxNegDelta_Pi__Volume.ToString().Length, @" ")  + bXmaxNegDelta_Pi__Volume
                         );
             sw.Close();
             sw.Dispose();
